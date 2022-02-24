8c8e855bd71992fb104a1f1884a9bc2e0b7a7d34
Spencer Mehta <spencermehta@gmail.com>
2022-02-24 21:07:14 +0000

    feat: wizz branding

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,62 +10,55 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Guess the word in 6 tries. After each guess, the color of the tiles will
-        change to show how close your guess was to the word.
+        Guess the origin-destination pair in 6 tries. After each guess, the
+        color of the tiles will change to show how close your guess was to the
+        route.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="W"
+          value="B"
           status="correct"
         />
-        <Cell value="E" />
-        <Cell value="A" />
-        <Cell value="R" />
-        <Cell value="Y" />
+        <Cell value="U" />
+        <Cell value="D" />
+        <Cell value="L" />
+        <Cell value="T" />
+        <Cell value="N" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter W is in the word and in the correct spot.
+        The letter W is in the route and in the correct spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="P" />
-        <Cell value="I" />
+        <Cell value="B" />
+        <Cell value="U" />
         <Cell
           isRevealing={true}
           isCompleted={true}
-          value="L"
+          value="D"
           status="present"
         />
-        <Cell value="O" />
+        <Cell value="L" />
         <Cell value="T" />
+        <Cell value="N" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter L is in the word but in the wrong spot.
+        The letter L is in the route but in the wrong spot.
       </p>
 
       <div className="flex justify-center mb-1 mt-4">
-        <Cell value="V" />
-        <Cell value="A" />
-        <Cell value="G" />
-        <Cell isRevealing={true} isCompleted={true} value="U" status="absent" />
-        <Cell value="E" />
+        <Cell value="B" />
+        <Cell value="U" />
+        <Cell value="D" />
+        <Cell isRevealing={true} isCompleted={true} value="L" status="absent" />
+        <Cell value="T" />
+        <Cell value="N" />
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        The letter U is not in the word in any spot.
-      </p>
-
-      <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
-        This is an open source version of the word guessing game we all know and
-        love -{' '}
-        <a
-          href="https://github.com/cwackerfuss/react-wordle"
-          className="underline font-bold"
-        >
-          check out the code here
-        </a>{' '}
+        The letter U is not in the route in any spot.
       </p>
     </BaseModal>
   )
